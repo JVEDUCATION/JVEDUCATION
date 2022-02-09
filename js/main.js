1360e747299a4b7efb50cf29e0d0e87650702522
JVEDUCATION <99245629+JVEDUCATION@users.noreply.github.com>
2022-02-09 12:44:11 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ var email=document.getElementById("email").value;
 var password=document.getElementById("password").value;
 if(email=="123@gmail.com"&& password=="123")
 {
-	window.open("#");
+	window.open("https://jveducation.github.io/JVEDUCATION/login.html");
 	return false;
 
 }
