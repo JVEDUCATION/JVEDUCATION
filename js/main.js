ff7fab96c5cc826610dae4aeea0dfdc83b23db17
JVEDUCATION <99245629+JVEDUCATION@users.noreply.github.com>
2022-02-16 10:15:37 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+function validate()
+{
+var email=document.getElementById("email").value;
+var password=document.getElementById("password").value;
+if(email=="123@gmail.com"&& password=="123")
+{
+	window.open("https://jveducaton2.weebly.com/");
+	return false;
+
+}
+else
+{
+	alert("Login Failed");
+}
+
+
+
+}
